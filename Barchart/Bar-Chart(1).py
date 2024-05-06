c93977465688678de63e97726f92b66246d6287f
saadsohail05 <saadsohail5104@gmail.com>
2024-05-06 21:08:23 +0500

    Barchart Branch Merged

---
+++ Barchart/Bar-Chart(1).py
@@ -0,0 +1,29 @@
+#1.	Top 5 Batters per season.
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_top_5_batters(season):
+    # Read the data from CSV
+    data = pd.read_csv('PSL.csv')
+    
+    season_data = data[data['season'] == season]
+    
+    batter_totals = season_data.groupby('Batter')['Batter runs'].sum()
+    
+    top_5_batters = batter_totals.sort_values(ascending=False).head(5)
+    
+    X = top_5_batters.index.tolist()
+    Y = top_5_batters.values.tolist()
+    
+    plt.bar(X, Y, color='g')
+    plt.title("Top 5 Batters in Season {}".format(season))
+    plt.xlabel("Batter")
+    plt.ylabel("Total Runs")
+    
+    plt.xticks(rotation=12)
+    
+    plt.show()
+
+# Take input for the season
+season = "2018/19" # you can change seasons here
+plot_top_5_batters(season)
---
+++ Barchart/Bar-Chart(2).py
@@ -0,0 +1,30 @@
+# 2.	Top 5 Bowlers per season.
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_top_bowlers_by_season(season):
+    data = pd.read_csv('PSL.csv')
+
+    season_data = data[data['season'] == season]
+
+    bowler_totals = season_data.groupby('Bowler')['Wickets'].sum()
+
+    top_5_bowlers = bowler_totals.sort_values(ascending=False).head(5)
+
+    X = top_5_bowlers.index.tolist()
+    Y = top_5_bowlers.values.tolist()
+
+    plt.bar(X, Y, color='b')
+    plt.title(f"Top 5 Bowlers in PSL Season {season}")
+    plt.xlabel("Bowler")
+    plt.ylabel("Total Wickets")
+
+    plt.xticks(rotation=10)
+
+    plt.show()
+
+# Take input for the season
+season_input = "2016/17"# you can tak input of every season
+
+# Plot the top five bowlers for the specified season
+plot_top_bowlers_by_season(season_input)
